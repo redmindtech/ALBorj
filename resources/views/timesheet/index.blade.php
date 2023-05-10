44834aca54144f410ee194e8ec8eaa37415eed74
redmindtech <redmindtech.2020@gmail.com>
2023-05-10 18:26:45 +0530

    devipriya changes

--- resources/views/timesheet/index.blade.php
+++ resources/views/timesheet/index.blade.php
@@ -12,7 +12,7 @@
                     <div class="card shadow">
                         <div class="card-header">
                             <div class="d-flex justify-content-between">
-                                <h4 class="font-weight-bold text-dark py">TimeSheet</h4>
+                                <h4 class="font-weight-bold text-dark py">Employee TimeSheet</h4>
                                 <div style="width:120px">
                                     <button type="button" class="btn btn-block btn-primary" onclick="handleDialog()">Add</button>
                                 </div>
